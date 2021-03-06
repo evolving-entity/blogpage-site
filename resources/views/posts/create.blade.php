059c543f4b4f7c33742485447e118ac3ede9a2e5
Gibril Darboe <gsdarboe1@gmail.com>
2021-03-06 22:51:16 +0000

    Create posts form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,5 +4,18 @@
 @endphp
 
 @section('content')
-    <h1>Create a Post!</h1>
+    <div class="posts-container px-4 py-5">
+        <h1>Create a Post!</h1>
+        <form action="PostsController@store" method="POST">
+            <div class="form-group">
+                <label for="title">Blog Title</label>
+                <input type="text" name="title" id="title" class="form-control" placeholder="What shall we name it?" >
+            </div>
+            <div class="form-group">
+                <label for="body">Blog Content</label>
+                <input type="textarea" name="body" id="body" class="form-control" placeholder="What is it about?" >
+            </div>
+            <button type="submit" class="btn btn-primary">Upload</button>
+        </form>
+    </div>
 @endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,9 @@
 @endphp
 
 @section('content')
-    <h1>{{$post->title}}</h1>
-    <small>{{$post->created_at}}</small>
-    <p>{{$post->body}}</p>
+    <div class="posts-container px-4 py-5">
+        <h1>{{$post->title}}</h1>
+        <small>{{$post->created_at}}</small>
+        <p>{{$post->body}}</p>
+    </div>
 @endsection
